aef0934276c290ad06a5c08b5576435b0bb903b5
farzanurifan <farzanurifan@gmail.com>
2019-07-07 12:43:22 +0700

    bat

---
+++ run_elmo_ori_3.bat
@@ -0,0 +1,18 @@
+call "C:\Users\Admin\Anaconda3\Scripts\activate.bat"
+cd "C:\Users\Admin\tugas-akhir"
+python elmo_terms.py 232 237
+python elmo_terms.py 237 242
+python elmo_terms.py 242 247
+python elmo_terms.py 247 252
+python elmo_terms.py 252 257
+python elmo_terms.py 257 262
+python elmo_terms.py 262 267
+python elmo_terms.py 267 272
+python elmo_terms.py 272 277
+python elmo_terms.py 277 282
+python elmo_terms.py 282 287
+python elmo_terms.py 287 292
+python elmo_terms.py 292 297
+python elmo_terms.py 297 302
+python elmo_terms.py 302 307
+python elmo_terms.py 307 312
---
+++ run_elmo_ori_7.bat
@@ -0,0 +1,18 @@
+call "C:\Users\Admin\Anaconda3\Scripts\activate.bat"
+cd "C:\Users\Admin\tugas-akhir"
+python elmo_terms.py 543 548
+python elmo_terms.py 548 553
+python elmo_terms.py 553 558
+python elmo_terms.py 558 563
+python elmo_terms.py 563 568
+python elmo_terms.py 568 573
+python elmo_terms.py 573 578
+python elmo_terms.py 578 583
+python elmo_terms.py 583 588
+python elmo_terms.py 588 593
+python elmo_terms.py 593 598
+python elmo_terms.py 598 603
+python elmo_terms.py 603 608
+python elmo_terms.py 608 613
+python elmo_terms.py 613 618
+python elmo_terms.py 618 623
